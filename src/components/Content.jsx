3d00d6ddef2a98a8a29541e44ee56e0ac769b684
cRistianMel0 <cRistianMel0@users.noreply.github.com>
2025-04-15 21:11:37 -0500

    Update functionality to get images and prompt text

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -26,24 +26,6 @@ export default function Content() {
         .then(data => setMemesData(data.data.memes))        
     }, [])
     
-    /*
-    * Challenge:
-    * Get an array of memes from the imgflip API as soon as
-    * this component renders for the first time.
-    * Check the imgflip documentation for the correct URL.
-    * Save the array of memes (not the whole response
-    * data) to state. (For this app, we'll randomly choose
-    * one of the memes from this array when the user clicks
-    * the "Get a new meme image" button, but we'll do that in
-    * a separate challenge.)
-    * Hint: for now, don't try to use an async/await function.
-    * Instead, use *
-    .then()' to resolve the promises
-    * from using 'fetch'. We'll learn why after th Reproducción aut
-    */
-
-
-
     return (
         <main>
             <div className="form">
